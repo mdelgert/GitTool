a2f708609626a7c7c78480870fd31bd5c6c3f777
Matthew Elgert <MElgert@jjkeller.com>
2021-09-24 12:40:43 -0400

    First checkin

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin:");
+            Console.WriteLine(ThisAssembly.Git.Commit);
+
+        }
+
+    }
+
+}
